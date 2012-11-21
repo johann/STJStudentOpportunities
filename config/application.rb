85fc8399c160ff4091e5b8a4cb2ca0ae9f01cf10
johannkerr <johannkerr@gmail.com>
2012-11-20 23:48:58 -0500

    hope this works

--- config/application.rb
+++ config/application.rb
@@ -72,7 +72,6 @@ module Myapp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    config.assets.initialize_on_precompile = false
-    rake assets:precompile
+    config.assets.initialize_on_precompile = false    
   end
 end
